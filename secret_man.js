e390fdd4c0c6f63da67903c6fcb23cb23f940506
minhaj <minhaj101011@gmail.com>
2024-09-16 19:44:59 +0600

    hide

---
+++ secret_man.js
@@ -0,0 +1,10 @@
+const API_KEY = 'AIzaSyBWkvsUZPm-t1KM8xWgrVyhwK09pS6jPpc';
+const academicSheetID = '1UH9j_332yT0M4BtmySqQuvJmqfJIf2b54iwuShL7dtk'
+const oneshotSheetID = '1eRcTTvRQLSZ0iRV2occU5FYoOZ2FOszOi3nuZddvIiU'; // now frpb 25 class sheet id
+const examSheetID = '1uowLk3nSTefCcwnq6fx4GDcM481mgWJJ6sWqeeS2los';
+const apiKey = 'AIzaSyBooe-Fdm9nJRv1buEzPA8ayyRPPXtPQbs';
+const spreadsheetId = '1-diTmHqzyhbZp-ruO-d_BZ7iEyv3tsQN7LzXfT0FQoM';//'1Sn0_s0_3KvlrrK90krhV9tUeax8yde-RVLNF10tHLqY'; // for results
+const scheduleSheetID = '1NLpaHHSWiwKmszSWX-Ivxxq2MOBS921VwljoRfuThSw';
+const noticeSheetID = '1hHs5pt0iXwzfR1PYkvMUdBLURwlQnt_RMmVjI1yUYkM';
+const routineSheetID = '1PO6MMMwQD436CLfCwazuyyrdV3pkmJh5yqO5lU8s7hk';
+
